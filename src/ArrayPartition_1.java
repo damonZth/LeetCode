d944a7d122f4951c274f651373f018474904d484
damonZth <822730025@qq.com>
2017-06-15 22:10:45 +0800

    2017.06.15 第一次创建leetcode仓库，用来存放练习的记录。

---
+++ src/ArrayPartition_1.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+/**
+ * Created by Damon on 2017/6/15.
+ * https://leetcode.com/problems/array-partition-i/#/description
+ *
+ * Given an array of 2n integers, your task is to group these integers into n pairs of integer,
+ * say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+ *
+ * Note:
+ * n is a positive integer, which is in the range of [1, 10000].
+ * All the integers in the array will be in the range of [-10000, 10000].
+ */
+public class ArrayPartition_1 {
+    public int arrayPairSum(int[] nums) {
+        /**
+         *数组中，取两个数形成（ai，bi）对，由（ai，bi）取最小值，使得该值尽可能大
+         * 思路：尽可能去数组中较大的值。先对数组进行排序，然后依次取两个数形成一对。
+         **/
+        int n = nums.length;
+        int sum = 0;
+        Arrays.sort(nums);//对数组进行排序
+        /**
+         * 排好序的数组中，每两个数中，偶数位的元素值较小，则可以直接选取数组偶数位的元素求和
+         **/
+        for(int i = 0; i < n; i++){
+            if(i%2 == 0){
+                sum += nums[i];
+            }
+        }
+        return sum;
+
+    }
+
+    public int min(int a,int b){
+        return (a > b) ? b : a;
+    }
+
+}
---
+++ src/ConstructtheRectangle.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Damon on 2017/6/15.
+ * https://leetcode.com/problems/construct-the-rectangle/#/description
+ *
+ * For a web developer, it is very important to know how to design a web page's size.
+ * So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page,
+ * whose length L and width W satisfy the following requirements:
+ * 1. The area of the rectangular web page you designed must equal to the given target area.
+ * 2. The width W should not be larger than the length L, which means L >= W.
+ * 3. The difference between length L and width W should be as small as possible.
+ * You need to output the length L and the width W of the web page you designed in sequence.
+ *
+ * Note:
+ * The given area won't exceed 10,000,000 and is a positive integer
+ * The web page's width and length you designed must be positive integers.
+ *
+ */
+public class ConstructtheRectangle {
+    public int[] constructRectangle(int area) {
+        int[] result = new int[2];
+        int n = (int)Math.sqrt(area);
+        for(int i = n; i <= area; i++){
+            int length = i;
+            int width = area/i;
+            if(length * width == area){
+                result[0] = length < width ? width : length;
+                result[1] = area/result[0];
+                return result;
+            }
+        }
+        return result;
+    }
+}
